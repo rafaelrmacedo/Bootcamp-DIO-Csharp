883b1fb15ac028cd0ce986cc1614f4d41fc38798
rafaelrmacedo <rafaelrodriguesmacedo2004@gmail.com>
2022-10-07 16:47:01 -0300

    knowing the datatypes & the DateTime

--- module 3/Program.cs
+++ module 3/Program.cs
@@ -1,3 +1,12 @@
-﻿using module_3.Models.Person.cs;
+﻿using module_3.models;
 
-Person person = new Pessoa();
+DateTime dateTime = DateTime.Now.AddMinutes(10);
+
+Console.WriteLine(dateTime//.ToString("dd/MM/yyyy")
+);
+
+
+
+// Person person = new Person();
+
+// person.Presentation();
