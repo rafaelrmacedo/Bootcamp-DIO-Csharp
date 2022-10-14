e56dbbc10bf25454a56fcfcd1a407182b0f14416
rafaelrmacedo <rafaelrodriguesmacedo2004@gmail.com>
2022-10-14 19:09:41 -0300

    challenge finished

---
+++ module3/module3Challenge/Models/Parking.cs
@@ -0,0 +1,65 @@
+namespace module3Challenge.Models
+{
+    public class Parking
+    {
+        private decimal inicialPrice = 0;
+        private decimal pricePerHour = 0;
+        private List<string> vehicle = new List<string>();
+        string licencePlate = "";
+
+        public Parking(decimal inicialPrice, decimal pricePerHour)
+        {
+            this.inicialPrice = inicialPrice;
+            this.pricePerHour = pricePerHour;
+        }
+
+        public void AddVehicle()
+        {
+            // TODO: Pedir para o usuário digitar uma licencePlate (ReadLine) e adicionar na lista "vehicle"
+            // *IMPLEMENTE AQUI*
+            Console.WriteLine("Type the licence plate to park the vehicle:");
+            licencePlate = Console.ReadLine();
+
+            vehicle.Add(licencePlate);
+        }
+
+        public void RemoveVehicle()
+        {
+            Console.WriteLine("Type the vehicle licence plate to remove:");
+
+            string licencePlate = Console.ReadLine();
+
+            // Verify if the vehicle exists
+            if (vehicle.Any(x => x.ToUpper() == licencePlate.ToUpper()))
+            {
+                Console.WriteLine("Type how much hours that vehicle was parked:");
+                int hours = Convert.ToInt32(Console.ReadLine());
+
+                decimal totalPrice = inicialPrice + pricePerHour * hours;
+
+                vehicle.Remove(licencePlate);
+
+                Console.WriteLine($"The vehicle {licencePlate} was removed and the total price is: $ {totalPrice}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, this vehicle isn't park here. Verify the if the licence plate was typed correctly.");
+            }
+        }
+
+        public void ListVehicle()
+        {
+            if (vehicle.Any())
+            {
+                Console.WriteLine("The park vehicles is:");
+
+                // Loop for show all vehicles in the list
+                vehicle.ForEach(Console.WriteLine);
+            }
+            else
+            {
+                Console.WriteLine("Don't have any parked cars here.");
+            }
+        }
+    }
+}
---
+++ module3/module3Challenge/Program.cs
@@ -0,0 +1,58 @@
+﻿using module3Challenge.Models;
+
+// Put the encoding to UTF8 for show accent (pt-BR)
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal inicialPrice = 0;
+decimal pricePerHour = 0;
+
+Console.WriteLine("Welcome to the Parking System!\n" +
+                  "Type an inicial price for the car that will park:");
+inicialPrice = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Now type the price per hour:"); pricePerHour = Convert.ToDecimal(Console.ReadLine());
+
+Parking parking = new Parking(inicialPrice, pricePerHour);
+
+string option = string.Empty;
+bool showMenu = true;
+
+// Realiza o loop do menu
+while (showMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Type your option:");
+    Console.WriteLine("1 - Register Vehicle");
+    Console.WriteLine("2 - Remove Vehicle");
+    Console.WriteLine("3 - List Vehicles");
+    Console.WriteLine("4 - Exit");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            parking.AddVehicle();
+            break;
+
+        case "2":
+            parking.RemoveVehicle();
+            break;
+
+        case "3":
+            parking.ListVehicle();
+            break;
+
+        case "4":
+            showMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+
+    Console.WriteLine("Press enter to continue:");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+Console.WriteLine("The software was closed.");
