9229eaf1f2512a2010ab398a7e3f9f643ce2b68a
rafaelrmacedo <rafaelrodriguesmacedo2004@gmail.com>
2022-12-05 19:27:24 -0300

    connection string

--- module6/Program.cs
+++ module6/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<CalendarContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
